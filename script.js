eaf8210c68058a2b27a7f00db53e7cb4e962b6cd
Gaurav Dhak <113551816+GauravDhak@users.noreply.github.com>
2026-06-13 12:46:37 +0530

    Add typing effect for text array in script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+const text = [
+"RTL Design",
+"FPGA Acceleration",
+"Hardware Security",
+"VLSI Engineering",
+"Digital Systems"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type() {
+
+if(count === text.length){
+count = 0;
+}
+
+currentText = text[count];
+letter = currentText.slice(0, ++index);
+
+document.getElementById("typing").textContent = letter;
+
+if(letter.length === currentText.length){
+
+count++;
+index = 0;
+
+setTimeout(type, 1500);
+
+}else{
+
+setTimeout(type, 80);
+
+}
+
+})();
